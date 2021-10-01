f20e4c870518e8040336a03c8d8731d90d6aafcc
nsadisha <nsadisha@gmail.com>
2021-10-01 16:13:39 +0530

    SQL query completed

--- src/main/java/com/rgsystem/report/reports/DailySalesReport.java
+++ src/main/java/com/rgsystem/report/reports/DailySalesReport.java
@@ -1,7 +1,7 @@
 package com.rgsystem.report.reports;
 
 import com.rgsystem.database.Database;
-import com.rgsystem.report.results.DailyReportResult;
+import com.rgsystem.report.results.DailySalesResult;
 import com.rgsystem.report.Period;
 import com.rgsystem.report.Report;
 import com.rgsystem.report.ReportResult;
@@ -19,8 +19,15 @@ public class DailySalesReport implements Report {
     public ReportResult getReport() throws Exception{
         ReportResult result;
         try{
-            String query = "SELECT * FROM orders";
-            result = new DailyReportResult(
+            //getting start date and end date as strings
+            String startDate = this.period.getStartDate();
+            String endDate = this.period.getEndDate();
+            //SQL query to execute
+            String query = "SELECT * FROM orders WHERE" +
+                    " date BETWEEN'" +startDate+ "' AND '" +endDate+ "' ORDER BY date ASC";
+
+            //Report result
+            result = new DailySalesResult(
                 database.executeQuery(query)
             );
         }catch(Exception e){
