41d9eeb7af443da6b62b492183e6558101e2eaef
nsadisha <nsadisha@gmail.com>
2021-10-02 20:52:08 +0530

    new custom exception

---
+++ src/main/java/com/rgsystem/database/QueryExecutionFailedException.java
@@ -0,0 +1,7 @@
+package com.rgsystem.database;
+
+public class QueryExecutionFailedException extends Exception{
+    public QueryExecutionFailedException(String message){
+        super(message);
+    }
+}
