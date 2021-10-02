df01598fcf5745e85b7ec2de05d1e03bdcf7c593
nsadisha <nsadisha@gmail.com>
2021-10-02 20:12:21 +0530

    interface deleted

--- src/main/java/com/rgsystem/report/ReportResult.java
+++
@@ -1,9 +0,0 @@
-package com.rgsystem.report;
-
-import com.rgsystem.report.results.NullReportException;
-
-import java.sql.ResultSet;
-
-public interface ReportResult {
-    ResultSet getResult() throws NullReportException;
-}
