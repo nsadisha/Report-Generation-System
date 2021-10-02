0c651bbba6e4c4e8b06b52aef071604412084f49
nsadisha <nsadisha@gmail.com>
2021-10-02 22:44:07 +0530

    fixed updated input hanndling

--- src/main/java/com/rgsystem/Main.java
+++ src/main/java/com/rgsystem/Main.java
@@ -1,7 +1,25 @@
 package com.rgsystem;
 
+import com.rgsystem.connection.DBConnection;
+import com.rgsystem.connection.SQLConnection;
+import com.rgsystem.database.Database;
+import com.rgsystem.database.SQLDatabase;
+import com.rgsystem.input.Inputs;
+import com.rgsystem.input.CommandLineInputs;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello project");
+        DBConnection connection = new SQLConnection(
+                "jdbc:mysql://localhost:3306/bookbae",
+                "root",
+                ""
+        );
+
+        Database database = new SQLDatabase();
+        Inputs inputs = new CommandLineInputs(args);
+        ReportGeneratorApp app = new ReportGeneratorApp(connection, database, inputs);
+
+        //start the app
+        app.execute();
     }
 }
--- src/main/java/com/rgsystem/ReportGeneratorApp.java
+++ src/main/java/com/rgsystem/ReportGeneratorApp.java
@@ -2,43 +2,42 @@ package com.rgsystem;
 
 import com.rgsystem.connection.DBConnection;
 import com.rgsystem.database.Database;
+import com.rgsystem.input.Inputs;
+import com.rgsystem.input.InvalidInputException;
 import com.rgsystem.report.Period;
 import com.rgsystem.report.Report;
 import com.rgsystem.report.ReportFactory;
-import com.rgsystem.report.results.ReportResult;
-
-import java.sql.ResultSet;
 
 public class ReportGeneratorApp {
     //database and connection
     private final DBConnection connection;
     private final Database database;
+    private final Inputs inputs;
 
-    //inputs
-    private final Period period;
-
-    public ReportGeneratorApp(DBConnection connection, Database database){
+    public ReportGeneratorApp(DBConnection connection, Database database, Inputs inputs){
         this.connection = connection;
         this.database = database;
-        this.period = new Period("2020-10-10", "2021-12-31");
+        this.inputs = inputs;
     }
 
     public void execute(){
         try{
             //connect to the database
             database.connect(connection.getConnection());
-            ReportFactory factory = new ReportFactory(this.database, this.period);
-            Report report = factory.getInstance("daily-sales");
-            ReportResult result = report.getSummaryReport();
-            ResultSet rows = result.getResult();
-            while (rows.next()){
-                System.out.println(
-                        rows.getString("date")+"  "+rows.getString("Transactions")+"  "+rows.getString("Net Sales")
-                );
-            }
+
+            Period period = new Period(
+                    this.inputs.getStartDate(),
+                    this.inputs.getEndDate()
+            );
+
+            ReportFactory factory = new ReportFactory(this.database, period);
+            String reportType = inputs.getReportType();
+            Report report = factory.getInstance(reportType);
 
         }catch(Exception e){
             System.out.println(e.getMessage());
+        } catch (InvalidInputException e) {
+            e.printStackTrace();
         }
     }
 }
