d54397a7396a27fee996da5a51d9e24fd9ccbe50
nsadisha <nsadisha@gmail.com>
2021-10-03 05:55:07 +0530

    full user report

--- src/main/java/com/rgsystem/report/reports/UserSignupsReport.java
+++ src/main/java/com/rgsystem/report/reports/UserSignupsReport.java
@@ -24,7 +24,7 @@ public class UserSignupsReport implements Report {
             String startDate = this.period.getStartDate();
             String endDate = this.period.getEndDate();
             //SQL query to execute
-            String query = "SELECT CAST(joined_date as date) 'Date', COUNT(*) 'New Users'" +
+            String query = "SELECT CAST(joined_date as date) 'Date', COUNT(*) 'New Users' " +
                     "FROM users " +
                     "WHERE joined_date BETWEEN '"+startDate+"' AND '"+endDate+"' " +
                     "GROUP BY CAST(joined_date as date) " +
@@ -49,18 +49,19 @@ public class UserSignupsReport implements Report {
             String startDate = this.period.getStartDate();
             String endDate = this.period.getEndDate();
             //SQL query to execute
-            String query = "SELECT CAST(date as date) 'Date', order_id 'Order ID', email 'Email', " +
-                    "total_price 'Total Revenue', status 'Status' " +
-                    "FROM orders " +
-                    "WHERE date BETWEEN '"+startDate+"' AND '"+endDate+"' " +
-                    "ORDER BY date DESC";
+            String query = "SELECT CAST(U.joined_date as date) 'Joined date', U.email 'Email', " +
+                    "CONCAT(fname,' ',lname) 'Name', contact 'Contact number', " +
+                    "CONCAT(A.line1,', ',A.line2,', ',A.city,'.') 'Address', A.province 'Province', A.zip 'Zip Code' " +
+                    "FROM users U JOIN user_addresses A ON U.email = A.email " +
+                    "WHERE U.joined_date BETWEEN '"+startDate+"' AND '"+endDate+"' " +
+                    "ORDER BY U.joined_date DESC";
 
             //Report result
             result = new ReportResult(
                     database.executeQuery(query)
             );
         }catch(Exception e){
-            throw new ReportGenerationFailedException("Summery report generation failed: "+e.getMessage());
+            throw new ReportGenerationFailedException("Full report generation failed: "+e.getMessage());
         }
 
         return result;
