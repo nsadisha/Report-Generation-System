69ecc713ecc419e652611265535e1b8065dc0ad5
tchamalsha <tharu.chamalsha@gmail.com>
2021-10-03 14:27:16 +0530

    add test case to subject

---
+++ src/main/Test/com/rgsystem/report/EmailBodyGeneratorTest.java
@@ -0,0 +1,13 @@
+package com.rgsystem.report;
+
+import junit.framework.TestCase;
+import org.junit.Test;
+
+public class EmailBodyGeneratorTest extends TestCase {
+
+    @Test
+    public void subject_should_not_be_nll(){
+
+    }
+
+}
